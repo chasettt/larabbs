d4c194b847c36a9173247c6e0ca419eb729d6990
tenghao <648850431@qq.com>
2022-06-21 17:48:22 +0800

    翻译队列化

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -57,4 +57,9 @@ class Topic extends Model
     {
         return $this->orderBy('created_at', 'desc');
     }
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
 }
