adb3693e173155450a3506ce9ad497e6851b8879
tenghao <648850431@qq.com>
2022-06-22 11:28:57 +0800

    admin

--- database/seeders/RepliesTableSeeder.php
+++ database/seeders/RepliesTableSeeder.php
@@ -3,10 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use App\Models\Reply;
+
 
 class RepliesTableSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     public function run()
     {
         Reply::factory()->times(1000)->create();
